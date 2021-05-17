6ecda6e576d3759f23e4d6a912117b232a8e7f54
Muhammed Hakan Kılıç <m-h-k140@hotmail.com>
2021-05-17 21:33:22 +0300

    I Added main page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <header>
+        <div className="logo">
+            <a href=""><img src="images/logo.png" alt="logo"/> <p>Smart Store</p> </a>
+        </div>
+        <div className="buttons">
+            <ul>
+                <li><a href=""> <p> About Us </p></a></li>
+                <li><a href=""> <p> Sign Up </p></a></li>
+                <li><a href=""> <p> Log In </p></a></li>
+            </ul>
+        </div>
+    </header>
+    <div className="content">
+        <div className="texts">
+            <h1>We Have a Solution</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet dignissimos sequi sed at consequatur fugiat magnam, quas ex accusamus dolorem eum similique impedit tempora modi obcaecati laborum reprehenderit. Exercitationem, aut.
+            </p>
+        </div>
+        <div className="image">
+            <img src="images/insanlar.jpg" alt="flat-illustration"/>
+        </div>
+
     </div>
+</div>
   );
 }
 
